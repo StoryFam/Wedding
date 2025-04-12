6c9175f0f6af4c8ef4994b4fb0a4c78d21adf0c5
CeeFamily <btstory97@gmail.com>
2025-04-12 00:15:00 -0400

    updated styles
    literally just updated some of larger screens before realizing support for desktop is abandoned for this project

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//date for the event
 const eventDate = new Date('2025-09-19').getTime();
-
-// Update the countdown every second
-const countdownInterval = setInterval(() => {
+// per second update
+const countdown = setInterval(() => {
     const now = new Date().getTime();
     const timeLeft = eventDate - now;
 
-    
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    // update display    
+    
     document.getElementById('days').innerHTML = days;
     document.getElementById('hours').innerHTML = hours;
     document.getElementById('minutes').innerHTML = minutes;
